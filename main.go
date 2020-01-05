bd601771fb4cf5823b7120adc5ba93e313dbbf64
Rob Hoelz <rob@hoelz.ro>
2020-01-04 21:03:59 -0600

    Add HTTP skeleton
    Just include the routes that gitit itself does

--- main.go
+++ main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+func NYI() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		http.Error(w, "Not yet implemented", http.StatusNotImplemented)
+	}
+}
+
 func main() {
+	// Global actions
+	http.HandleFunc("/_activity", NYI())
+	http.HandleFunc("/_categories", NYI())
+	http.HandleFunc("/_index", NYI())
+	http.HandleFunc("/_login", NYI())
+	http.HandleFunc("/_logout", NYI())
+	http.HandleFunc("/_random", NYI())
+	http.HandleFunc("/_upload", NYI())
+	http.HandleFunc("/_search", NYI())
+	http.HandleFunc("/_go", NYI())
+
+	// XXX can take a ?printable param
+	http.HandleFunc("/", NYI())
+
+	// Page-specific actions
+	http.HandleFunc("/_delete", NYI())
+	http.HandleFunc("/_discuss", NYI())
+	http.HandleFunc("/_edit", NYI())
+	http.HandleFunc("/_history", NYI())
+	http.HandleFunc("/_showraw", NYI())
+
+	log.Println("Listening on localhost:8081...")
+	log.Fatal(http.ListenAndServe("localhost:8001", nil))
 }
