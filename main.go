a3797672a503cd30b9244b0b8eae74307a86844a
Rob Hoelz <rob@hoelz.ro>
2020-01-04 21:39:49 -0600

    Flesh out root page handler a bit
    Stub out some details like Git access and markdown for now, but whatever

--- main.go
+++ main.go
@@ -3,12 +3,71 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func NYI() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "Not yet implemented", http.StatusNotImplemented)
 	}
+}
+
+type gititError interface {
+	error
+
+	StatusCode() int
+	Status() string
+}
+
+type gitBlob struct {
+	Timestamp time.Time
+	Hash      string
+	Contents  string
+}
+
+func gitGetBlob(path string) (*gitBlob, error) {
+	blob := &gitBlob{
+		Timestamp: time.Now(),                                 // XXX STUB
+		Hash:      "1d9616855a130da2cd0665f79139f6d7853595b1", // XXX STUB
+		Contents:  "lorem ipsum\n",
+	}
+
+	return blob, nil
+}
+
+// XXX STUB
+func renderMarkdown(markdown string) (string, error) {
+	return markdown, nil
+}
+
+// XXX can take a ?printable param
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	if path == "/" {
+		path = "Front Page"
+	}
+
+	blob, err := gitGetBlob(path)
+	if err != nil {
+		if appErr, ok := err.(gititError); ok {
+			// XXX write out an HTML page that shows the error message?
+			http.Error(w, appErr.Status(), appErr.StatusCode())
+		} else {
+			log.Println(err.Error()) // XXX show this on a page in debug mode or something?
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Header().Add("ETag", blob.Hash)
+	renderedMarkdown, err := renderMarkdown(blob.Contents)
+	if err != nil {
+		log.Println(err.Error()) // XXX show this on a page in debug mode or something?
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	http.ServeContent(w, req, "", blob.Timestamp, strings.NewReader(renderedMarkdown))
 }
 
 func registerHandlers() {
@@ -23,8 +82,7 @@ func registerHandlers() {
 	http.HandleFunc("/_search", NYI())
 	http.HandleFunc("/_go", NYI())
 
-	// XXX can take a ?printable param
-	http.HandleFunc("/", NYI())
+	http.HandleFunc("/", rootHandler)
 
 	// Page-specific actions
 	http.HandleFunc("/_delete", NYI())
