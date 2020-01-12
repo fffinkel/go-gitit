b7927cbb1f41bf85035dd7a40f8489e26fdd2bf7
Rob Hoelz <rob@hoelz.ro>
2020-01-11 20:28:09 -0600

    Move global and per-test setup into their own parts
    ...plus clone the wikidata repo as part of per-test setup

--- main_test.go
+++ main_test.go
@@ -2,13 +2,60 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
+type testState struct {
+	originalWD string
+	tempdir    string
+}
+
+func (state *testState) teardown() {
+	err := os.Chdir(state.originalWD)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = os.RemoveAll(state.tempdir)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func setup() *testState {
+	tempdir, err := ioutil.TempDir("", "tmp-gogitit-")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	_, err = gitInvoke("clone", "wikidata", tempdir+"/wikidata")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	originalWD, err := os.Getwd()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = os.Chdir(tempdir)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return &testState{
+		originalWD: originalWD,
+		tempdir:    tempdir,
+	}
+}
+
 func TestRoot200(t *testing.T) {
-	registerHandlers()
+	state := setup()
+	defer state.teardown()
 
 	req, _ := http.NewRequest("GET", "/", nil)
 	rr := httptest.NewRecorder()
@@ -21,4 +68,10 @@ func TestRoot200(t *testing.T) {
 		fmt.Println(res.StatusCode)
 		t.Errorf("no dice")
 	}
+}
+
+func TestMain(m *testing.M) {
+	registerHandlers()
+
+	os.Exit(m.Run())
 }
